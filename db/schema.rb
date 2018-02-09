7d5510d23a9cd0df864b03bed9261b2dd899b0d0
dendomenko <dendomenko@gmail.com>
2018-02-09 11:16:18 +0200

    create ticket and user tables and models. Set all associations related to ticket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208163703) do
+ActiveRecord::Schema.define(version: 20180209085839) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -28,8 +28,28 @@ ActiveRecord::Schema.define(version: 20180208163703) do
     t.index ["route_id"], name: "index_stations_on_route_id"
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "train_id"
+    t.integer "user_id"
+    t.integer "start_station_id"
+    t.integer "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
+    t.index ["train_id"], name: "index_tickets_on_train_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
