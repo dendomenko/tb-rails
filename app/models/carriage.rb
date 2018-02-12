b2e65378658d3afd42016fa09eaf5cd6aba2b747
dendomenko <dendomenko@gmail.com>
2018-02-12 11:02:53 +0200

    fix comments to PR

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,8 +1,7 @@
 class Carriage < ApplicationRecord
   belongs_to :train
 
-  validates :top_seats, presence: true
-  validates :bottom_seats, presence: true
+  validates :top_seats, :bottom_seats, presence: true
 
   scope :coupes, -> { where(type: 'Coupe') }
   scope :economs, -> { where(type: 'Econom') }
