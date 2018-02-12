20ac4c8776eaefea56e1320308c5b08f31363922
dendomenko <dendomenko@gmail.com>
2018-02-12 13:16:50 +0200

    finish part 1-2. From now you can add station number on route in connection table. Added scope order_by_number to Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,8 @@ class Station < ApplicationRecord
   has_many :stations_routes
   has_many :routes, through: :stations_routes
   has_many :trains, inverse_of: :current_station, foreign_key: 'current_station_id'
+
+  validates :name, presence: true
+
+  scope :order_by_number, -> { select('stations.*, stations_routes.number').joins(:stations_routes).order('stations_routes.number').uniq }
 end
