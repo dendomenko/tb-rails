766641ea5ec4dce6b85c132fa8bf0bc0a72938aa
dendomenko <dendomenko@gmail.com>
2018-02-13 15:44:03 +0200

    Finish search trains by stations

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,8 +4,8 @@ class SearchesController < ApplicationController
   end
 
   def search_route
-    start_station = Station.find(params[:start_station_id])
-    end_station = Station.find(params[:end_station_id])
-    # @route = Route.fins
+    @start_station = Station.find params[:start_station_id]
+    @end_station = Station.find params[:end_station_id]
+    @routes = Route.find_by_stations(params[:start_station_id], params[:end_station_id])
   end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,6 @@
 class Route < ApplicationRecord
   has_many :trains
-  has_many :stations_routes
+  has_many :stations_routes, dependent: :destroy
   has_many :stations, through: :stations_routes
 
   validates :name, presence: true
@@ -11,6 +11,16 @@ class Route < ApplicationRecord
   def first_station
     stations.first
   end
+
+  scope :find_by_stations, ->(start_station_id, end_station_id) {
+    route_ids = joins(:stations_routes)
+                .where(['stations_routes.station_id = ? ', start_station_id])
+                .pluck(:route_id)
+    joins(:stations_routes)
+      .where(['stations_routes.station_id = ? ' \
+              'AND stations_routes.route_id IN (?)',
+              end_station_id, route_ids]).uniq
+  }
 
   private
 
