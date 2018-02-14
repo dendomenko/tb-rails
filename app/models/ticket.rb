b792d8e7794f198e11526f473485895f7257a408
dendomenko <dendomenko@gmail.com>
2018-02-14 17:26:47 +0200

    Add TicketsMailer and send mail when buy ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,9 +8,19 @@ class Ticket < ApplicationRecord
                              foreign_key: 'start_station_id'
   belongs_to :end_station, class_name: 'Station', foreign_key: 'end_station_id'
 
+  after_create :send_notification
+
   validates :first_name, :last_name, :middle_name, :passport, presence: true
 
   def full_name
     "#{first_name} #{last_name} #{middle_name}"
+  end
+
+  def route_name
+    "#{start_station.name} - #{end_station.name}"
+  end
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
   end
 end
