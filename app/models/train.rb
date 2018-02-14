2cb2ecfb88e95520a5e4f5e56032b05c4c8180eb
dendomenko <dendomenko@gmail.com>
2018-02-14 13:32:46 +0200

    Localized errors for validations

--- app/models/train.rb
+++ app/models/train.rb
@@ -8,6 +8,8 @@ class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'Station',
                                foreign_key: 'current_station_id'
 
+  validates :number, presence: true
+
   def count_seats(type, seat_type)
     carriages.where(type: type).sum(seat_type)
   end
