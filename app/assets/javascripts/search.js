e4aac83baf9a1b3959ff87ef5163b62cc4616467
dendomenko <dendomenko@gmail.com>
2018-02-15 16:02:33 +0200

    move station validations into function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,13 +2,18 @@ $(document).on('turbolinks:load', function(){
     $('.find').click(function(){
         start = $('#start_station_id').val();
         end = $('#end_station_id').val();
-        if (!start || !end) {
-           alert('Choose station!');
-           return false;      
-        }
-        if (start == end) {
-            alert('Choose another end station');
-            return false;
-        }
+        if (!validatie_stations(start, end)) {return false;}
     });
 });
+
+function validatie_stations(start, end) {
+    if (!start || !end) {
+        alert('Choose station!');
+        return false;      
+     }
+    if (start == end) {
+        alert('Choose another end station');
+        return false;
+    }
+    return true;
+}
